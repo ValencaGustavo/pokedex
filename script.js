b4b2e7fa0b125ec34d2a8d94a780f7837b265801
Gustavo Valença Marcilio <workgustavo10@gmail.com>
2024-08-09 17:00:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-function buscar() {
-    var entrada = document.getElementById('entrada').value.toLowerCase();
+function buscar(){
+    var entrada = document.getElementById("entrada").value.toLowerCase()
 
-    var url = "https://pokeapi.co/api/v2/pokemon/" + entrada;
+    var url = "https://pokeapi.co/api/v2/pokemon/" + entrada + "";
 
-    var contador = 0
-    if(entrada.length < 1) {
-        entrada = contador
-    }
+    fetch(url).then(response => response.json()
+).then(data => {var tela = document.getElementById('tela')
+    
+    tela.innerHTML =
+    
+    '<h2>' + data.name + '</h2>'   
 
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        var tela = document.getElementById('tela');
-        
-        tela.innerHTML =
-        '<h2>' + data.name + '</h2>' +
-        '<img class="pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/' + data.id + '.gif" > ' +
-        '<img class="pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/back/' + data.id + '.gif" > <br> <br>' +
-        '<p> Id: ' + data.id + '</p>' +
-        '<p> Tipo: ' + data.types.map(type => type.type.name).join(', ') + '</p>' +
-        '<p> Habilidades: ' + data.abilities.map(ability => ability.ability.name).join(', ') + '</p>';
++' <img class="pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/'+data.id+'.gif";> '
 
-        document.getElementById('entrada').value = '';
-    })
-    .catch(erro => {
-        var tela = document.getElementById('tela');
-        tela.innerHTML = '<p> Pokemon não encontrado, erro: ' + erro + '</p>';
-    });
++' <img class="pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/back/'+data.id+'.gif";> '
+
++ '<p> Id: ' + data.id + '</p>'
++ '<p> Tipo: ' + data.types.map(type => type.type.name) + '</p>'
++ '<p> Habilidades: ' + data.abilities.map(ability => ability.ability.name).join(', ') + '</p>'
+document.getElementById("entrada").value = ''
+
+ }).catch(erro => {
+    var tela = document.getElementById ('');
+    tela.innerHTML = '<p>Pokémon não encontrado, erro' + erro
+ })
 }
